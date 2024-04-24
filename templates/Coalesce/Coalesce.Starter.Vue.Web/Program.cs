db4b6e27ee556c725c7cac78b935fc334812fea4
Benjamin Michaelis <git@relay.benjamin.michaelis.net>
2024-04-24 09:40:51 -0700

    refactor: AddDbContext -> AddDbContextFactory for coalesce

--- templates/Coalesce/Coalesce.Starter.Vue.Web/Program.cs
+++ templates/Coalesce/Coalesce.Starter.Vue.Web/Program.cs
@@ -32,7 +32,7 @@ builder.Configuration
 
 IServiceCollection services = builder.Services;
 
-services.AddDbContext<AppDbContext>(options => options
+services.AddDbContextFactory<AppDbContext>(options => options
     .UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"), opt => opt
         .EnableRetryOnFailure()
         .UseQuerySplittingBehavior(QuerySplittingBehavior.SplitQuery)
